4127b7502c453f7a1b37496022aa06cbd5ca8228
rishabh.. <rishabhtripathi2004@gmail.com>
2026-02-24 13:45:01 +0530

    add google analytics tag

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-
+import Script from "next/script";
 export const metadata = {
   title: "ResumeCraft Pro",
   description: "Build beautiful, professional resumes with live preview",
@@ -8,6 +8,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+  async
+  src="https://www.googletagmanager.com/gtag/js?id=G-EBK1SC2J89"
+  strategy="afterInteractive"
+/>
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-EBK1SC2J89');
+  `}
+</Script>
+      </head>
       <body className="antialiased">
         {children}
         <div id="print-root" style={{ display: "none" }} />
